3a5dd0907b685ad471753d6918999c8f0f906e71
Darcy Douglas <darcydouglas@users.noreply.github.com>
2023-10-08 22:09:27 +1100

    Added start page heading and subheading. Now navigates to home.html when video ends

--- pages/Start/start.js
+++ pages/Start/start.js
@@ -1,8 +1,14 @@
-var video = document.getElementById("home_space_jump");
-var playButton = document.getElementById("video_start_button");
+document.addEventListener('DOMContentLoaded', function() {
+    var video = document.getElementById("home_space_jump");
+    var playButton = document.getElementById("video_start_button");
+    var mainHeading = document.getElementById("main-heading");
+    var subHeading = document.getElementById("sub-heading");
 
-function playVideo() {
-    video.play();
-}
+    function playVideo() {
+        video.play();
+        mainHeading.style.display = 'none';  // Hide main heading
+        subHeading.style.display = 'none';   // Hide sub heading
+    }
 
-playButton.addEventListener('click', playVideo);
+    playButton.addEventListener('click', playVideo);
+});
